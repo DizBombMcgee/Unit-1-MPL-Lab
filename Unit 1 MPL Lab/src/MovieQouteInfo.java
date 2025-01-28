26e2f91ebca3b0aebd218569ea4dc54a9b2ab75b
Dillon Bruzzano <dillon.bruzzano@gmail.com>
2025-01-27 20:12:19 -0500

    Add files via upload

---
+++ Unit 1 MPL Lab/src/MovieQouteInfo.java
@@ -0,0 +1,16 @@
+// ******************************************************************** 
+// File Name: MovieQouteInfo.java
+// Author: Dillon Bruzzano
+// Purpose: To qoute a movie, say what movie it is from, 
+//          the character who said it, and the year movie was released
+// ******************************************************************** 
+public class MovieQouteInfo 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Life is like a box of chocolates, you never know what you're gonna get.");
+        System.out.println("Forrest Gump (Movie Title)");
+        System.out.println("Forrest Gump (Character)");
+        System.out.println("1994");
+    }
+}
---
+++ Unit 1 MPL Lab/src/SongLyrics.java
@@ -0,0 +1,17 @@
+// ******************************************************************** 
+// File Name: SongLyrics.java
+// Author: Dillon Bruzzano
+// Purpose: To provide four lines of my favorite song
+// ******************************************************************** 
+public class SongLyrics 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Sunshine daydream");
+        System.out.println("Walking through the tall trees");
+        System.out.println("Going where the wind blows");
+        System.out.println("Blooming like a red rose");
+    }
+    
+}
